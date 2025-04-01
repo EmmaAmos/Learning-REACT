c195ce81bd6b4f6b9b12e0b3047717cce0ad71e9
EmmaAmos <elovestodraw@gmail.com>
2025-04-01 14:57:01 -0400

    trying dynamtic naming

--- LearningReactProject/src/Message.tsx
+++ LearningReactProject/src/Message.tsx
@@ -1,6 +1,18 @@
+const creator = 'Emma';
+const husband = 'Pablo';
+
+
 function Message(){
-    return <h1>Hello There!</h1>
+    return <h1>Hello There!</h1>;
+    
+}
+
+function WelcomMess (){
+    <h2>{creator} and {husband}, this message is dynamically made!</h2>;
 }
 
 
+
+
 export default Message;
+export default WelcomMess;
