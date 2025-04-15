6f03b82d5d2dbbfbfa28a967a6224b038a6ab1f4
EmmaAmos <elovestodraw@gmail.com>
2025-04-15 11:06:03 -0400

    learning mapping

--- LearningReactProject/src/components/ListGroup.tsx
+++ LearningReactProject/src/components/ListGroup.tsx
@@ -1,12 +1,29 @@
+import { Fragment } from "react";
+
+
 function ListGroup() {
+
+  const items =[
+    'New York',
+    'San Fan',
+    'Toykyo',
+    'London',
+    'Paris'
+  ];
+
+  items.map(item => <li>{item}</li>);
+
     return(
-      <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-      </ul>
+      <Fragment>
+        <h1>Testing H1</h1>
+        <ul className="list-group">
+          <li className="list-group-item">An item</li>
+          <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">A fourth item</li>
+          <li className="list-group-item">And a fifth one</li>
+        </ul>
+      </Fragment>
     );
 }
 
