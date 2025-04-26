b0785677e8396301ee7b2f6377837a5c1174d79e
EmmaAmos <elovestodraw@gmail.com>
2025-04-26 17:00:04 -0400

    learning states

--- LearningReactProject/src/components/ListGroup.tsx
+++ LearningReactProject/src/components/ListGroup.tsx
@@ -1,9 +1,9 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 
 function ListGroup() {
 
-  const items =[
+  let items =[
     'New York',
     'San Fan',
     'Toykyo',
@@ -11,19 +11,30 @@ function ListGroup() {
     'Paris'
   ];
 
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
 
 //This takes the list of 'items' and converts that into a <li> element.
 // The items {} renders items dynamticlly, including the error message if nothing is found.
 
     return(
-      <Fragment>
-        <h1>Testing H1</h1>
-        {items.length === 0 && <p>No items</p> }
-        <ul className="list-group">
-            {items.map((item) => <li className="list-group-item" key={item} onClick={(event) => console.log(event)}>{item}</li>)}
-        </ul>
-      </Fragment>
-    );
+        <Fragment>
+          <h1>Testing H1</h1>
+          {items.length === 0 && <p>No items</p>}
+          <ul className="list-group">
+            {items.map((item, index) => ( // Look here! We're getting the index
+              <li
+                className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+                key={item}
+                onClick={() => {setSelectedIndex(index)}}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      );
 }
 
 export default ListGroup;
